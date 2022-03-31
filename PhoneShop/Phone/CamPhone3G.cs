5299c4a5c06c82769bce3f1b4bd0d6b82c4d7165
Афлятунов Марсель (Aflyatunov_MR) <Aflyatunov_MR@directum.ru>
2022-03-31 11:09:43 +0500

    some

--- PhoneShop/Phone/CamPhone3G.cs
+++ PhoneShop/Phone/CamPhone3G.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhoneShop
 {
@@ -39,7 +35,7 @@ namespace PhoneShop
     #endregion
 
     #region Конструкторы
-    
+
     /// <summary>
     /// Конструктор.
     /// </summary>
